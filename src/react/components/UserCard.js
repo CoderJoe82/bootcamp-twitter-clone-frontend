168db43725daf41cb351d99718b98e7484ad5589
CoderJoe82 <coderjoe82@gmail.com>
2020-03-25 16:36:29 -0400

    finished removing unused ref's/variables

--- src/react/components/UserCard.js
+++ src/react/components/UserCard.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
@@ -7,7 +6,6 @@ import CardMedia from '@material-ui/core/CardMedia';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
-import clsx from 'clsx';
 import Collapse from '@material-ui/core/Collapse';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import IconButton from '@material-ui/core/IconButton';
@@ -18,21 +16,6 @@ import { newProfilePhoto } from "../../redux/newProfilePhoto"
 import { connect } from "react-redux"
 import { domain } from "../../redux/helpers"
 
-// const useStyles = makeStyles(theme => ({
-//   root: {
-//     maxWidth: 345,
-//   },
-//   expand: {
-//     transform: 'rotate(0deg)',
-//     marginLeft: '2',
-//     transition: theme.transitions.create('transform', {
-//       duration: theme.transitions.duration.shortes
-//     }),
-//   },
-//   expandOpen: {
-//     transform: 'rotate(180deg)',
-//   },
-// }));
 
 
 class UserCard extends React.Component {
